7b7c2bbd76c47ace71db305ee5dcd355de30d39e
Zellmer <Zellmer@users.noreply.github.com>
2021-12-20 18:42:26 +0100

    Update tryhackme.js

--- tryhackme.js
+++ tryhackme.js
@@ -2,19 +2,31 @@
 function CSSClass(){//nur einmal laden!
   $("head").append('<style type="text/css"></style>');
   var new_stylesheet = $("head").children(':last');
-  let css = '.greenBorder{color:var(--hacker-green);border-left:5px solid var(--hacker-green);border-right:5px solid var(--hacker-green)}';
-  css += '.redBorder{border-left:5px solid var(--cool-red);border-right:5px solid var(--cool-red);opacity:1!important}';
+  let css = '.greenBorder{color:var(--hacker-green);border-left:5px solid var(--hacker-green);border-right:5px solid var(--hacker-green);margin-bottom:0px}';
+  css += '.redBorder{border-left:5px solid var(--cool-red);border-right:5px solid var(--cool-red);opacity:1!important;margin-bottom:0px}';
   css += '.bg-danger{background-color:var(--cool-red)!important;}';
+  //css += '.room-completed{background-color:#fff;padding:0.1em;border-radius:0.2em;}';
+  css += '.room-F{background-color:navajowhite;opacity: 0.8;}';
+  
   new_stylesheet.html(css);
 }
 function FancyTask(){
   $("div .card[id^=task]").removeClass('card')
-  $(".hacker-green").closest('[id^=task]').children().css('margin-bottom','0px').addClass('greenBorder').removeClass('redBorder')
+  $(".hacker-green").closest('[id^=task]').children().addClass('greenBorder').removeClass('redBorder')
   $(".red").closest('[id^=task]').children().addClass('redBorder')
+}
+function FancyPractice(){
+  $(".path-tasks-grid").addClass('redBorder')
+  $(".hacker-green").closest('.path-tasks-grid').addClass('greenBorder').removeClass('redBorder')
+}
+function FancyRooms(){
+  $(".room-completed").closest('.room-main').addClass('room-F')
 }
 
 function jload(){
   FancyTask()
+  FancyPractice()
+  FancyRooms()
 }
 
 CSSClass()//nur einmal laden!
